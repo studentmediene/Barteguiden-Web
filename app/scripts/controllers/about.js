f86f99aa93d4c5f08cdfbd3c064394db7af27ecf
Martin Thoresen <martinhath@gmail.com>
2014-09-07 17:01:59 +0200

    Første commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name barteguidenWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the barteguidenWebApp
+ */
+angular.module('barteguidenWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name barteguidenWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the barteguidenWebApp
+ */
+angular.module('barteguidenWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
