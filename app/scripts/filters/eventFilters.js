65d7ca8394ee2559d5a15c40ba05a0d9fabd4f99
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-10-19 21:20:43 +0200

    Refactoring of filters, splitted in two separate files.

--- app/scripts/filters/eventFilters.js
+++ app/scripts/filters/eventFilters.js
@@ -3,28 +3,6 @@
 
 
 angular.module('barteguidenWebApp.filters')
-  .filter('isFreeEvent', function() {
-    return function(price) {
-      if(price === 0) {
-        return 'Gratis';
-      }
-      else if(price === undefined || price === null) {
-          return 'Pris ikke oppgitt';
-      }
-      else {
-        return price.toString() + ' kr';
-      }
-    };
-
-  })
-  .filter('isFreeForAll', function() {
-    return function(ageLimit) {
-      if(ageLimit === 0 || ageLimit === null) {
-        return 'Tillatt for alle';
-      }
-      return ageLimit + ' år';
-    };
-  })
   .filter('filterByPrice', function() {
     return function(events, wantedPrices) {
       var filtered = [];
@@ -88,30 +66,4 @@ angular.module('barteguidenWebApp.filters')
 
       return filtered;
     };
-  })
-  .filter('slugify', function() {
-    return function(text) {
-      return text.toString().toLowerCase()
-        .replace('ø', 'oe')
-        .replace('å', 'aa')
-        .replace('æ', 'ae')
-        .replace(/\s+/g, '-')        // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')   // Remove all non-word chars
-        .replace(/\-\-+/g, '-')      // Replace multiple - with single -
-        .replace(/^-+/, '')          // Trim - from start of text
-        .replace()
-        .replace(/-+$/, '');         // Trim - from end of text
-    };
-  })
-  .filter('cutText', function() {
-    return function(text, n) {
-      var shortText = text.substr(0, n);
-
-      if (/^\S/.test(text.substr(n))) {
-        var shortText = shortText.replace(/\s+\S*$/, "");
-        return shortText === text ? shortText : shortText + '[...]';
-      }
-      return shortText === text ? shortText : shortText + '[...]';
-    };
   });
-
