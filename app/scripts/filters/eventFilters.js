dc3f09fc07fb15fb28352dfc7276c107972f9755
Anna Kastet <annakastet@gmail.com>
2015-02-11 20:46:45 +0100

    Improved lodash-implementation

--- app/scripts/filters/eventFilters.js
+++ app/scripts/filters/eventFilters.js
@@ -1,77 +1,52 @@
 'use strict';
 /*global _:false */
 
-
-
-
 angular.module('barteguidenWebApp.filters')
   .filter('filterByPrice', function() {
     return function(events, wantedPrices) {
-      var filtered = [];
-
-      if(wantedPrices.length === 0) {
+      if(wantedPrices.length === 0) { //No options chosen. Return entire list of events.
         return events;
       }
 
-      for(var i = 0; i < events.length; i++) {
-        var eventPrice = events[i].price;
-
-        if(eventPrice > 0 || eventPrice === null) {
+      return _.filter(events, function (event) { //Returned filtered event
+        var eventPrice = event.price;
+        if (eventPrice > 0 || eventPrice === null) {
           eventPrice = -1; //-1 means paid event, 0 is free
           // has to be done due to different input
         }
+        return _.include(_.pluck(wantedPrices, 'id'), eventPrice);
+      });
 
-        for(var j = 0; j < wantedPrices.length; j++) {
-          if(wantedPrices[j].price === eventPrice) {
-            filtered.push(events[i]);
-          }
-        }
 
-      }
-
-      return filtered;
     };
   })
+
   .filter('filterByCategory', function() {
     return function(events, wantedCategories) {
-      var filtered = [];
-
-      if(wantedCategories.length === 0) {
+      if(wantedCategories.length === 0) { //No options chosen. Return entire list of events.
         return events;
       }
 
-      for(var i = 0; i < events.length; i++) {
-        for(var j = 0; j < wantedCategories.length; j++) {
-          if(wantedCategories[j].id === events[i].categoryID) {
-            filtered.push(events[i]);
-          }
-        }
-      }
-      return filtered;
+      return _.filter(events, function (event) { //Returned filtered events
+        return _.include(_.pluck(wantedCategories, 'id'), event.categoryID);
+      });
     };
   })
+
   .filter('filterByAgeLimit', function() {
-    return function(events, wantedAges) {
-      var filtered = [];
-
-      if(wantedAges.length === 0) {
+    return function (events, wantedAges) {
+      if (wantedAges.length === 0) {  //No options chosen. Return entire list of events.
         return events;
       }
-
-      for(var i = 0; i < events.length; i++) {
-        for(var j = 0; j < wantedAges.length; j++) {
-          if(wantedAges[j].id === events[i].ageLimit) {
-            filtered.push(events[i]);
-          }
-        }
-      }
-
-      return filtered;
+      return _.filter(events, function (event) {
+        return _.include(_.pluck(wantedAges,'id'), event.ageLimit);
+      });
     };
   })
+
   .filter('filterByDate', function() {
     return function(events, date) {
-      if(date === undefined) {
+      if(date === undefined) {  //No options chosen. Return entire list of events.
         return events;
       }
       return _.filter(events, function(e) {
@@ -99,4 +74,5 @@ angular.module('barteguidenWebApp.filters')
       return;
     });
   }]);
+
 
