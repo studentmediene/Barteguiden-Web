c2aabda6ee8e7a84ca4ae04f6e8b3b187fb73dde
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-03-19 20:32:44 +0100

    Fix dependencies and a small issue

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,10 +35,8 @@ angular
   .module('barteguidenWebApp', [
     'ngRoute',
     'ngSanitize',
-    'uiGmapgoogle-maps',
-    'ui.bootstrap',
     'angular-loading-bar',
-    'google-maps',
+    'uiGmapgoogle-maps',
     'ui.bootstrap',
     'angulike',
     'barteguidenWebApp.controllers',
